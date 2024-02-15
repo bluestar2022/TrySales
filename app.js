9419f53484c4bb5f0305983a8fbf2998464d6739
M H Y <bluestar2022@gmail.com>
2024-02-15 21:39:31 +0300

    get back to my latest backup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const expressHHH = require('express');
 const app = expressHHH();
 const PORT = 3000;
 const mongooseHHH = require('mongoose');
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send('Hello Hisham!');
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
 // });
 
 mongooseHHH
-    .connect('')
+    .connect(process.env.MongoDB_ConnectionString)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`connection to mongo db is successful: http://localhost:${PORT}`);
